8055abb52f2efed010c3c7b66e3b24808a33c55a
tingtinghsu <tingtinghsu@Hui-TingdeMacBook-Pro.local>
2023-05-03 07:42:51 +0800

    Refactor: Replace the conditional logic on price code

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -15,4 +15,29 @@ class Movie
 
   # movie = Movie.new("super mario", 2)
   # => #<Movie:0x00000001145d0cb8 @price_code=2,#  @title="super mario">
+
+  def charge(days_rented)
+    # determine amounts for each line
+    result = 0
+    case price_code
+      # when Movie::REGULAR
+    when 0
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+      # when Movie::NEW_RELEASE
+    when 1
+      result += days_rented * 3
+      # when Movie::CHILDRENS
+    when 2
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
+
+  def frequent_renter_points(days_rented)
+    # add bonus for a two day new release rental
+    # price_code == (Movie.NEW_RELEASE && days_rented > 1) ? 2 : 1
+    price_code == (1 && days_rented > 1) ? 2 : 1
+  end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -14,27 +14,10 @@ class Rental
   #  @movie=#<Movie:0x00000001145d0cb8 @price_code=2, @title="super mario">>
 
   def charge
-    # determine amounts for each line
-    result = 0
-    case movie.price_code
-      # when Movie::REGULAR
-    when 0
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-      # when Movie::NEW_RELEASE
-    when 1
-      result += days_rented * 3
-      # when Movie::CHILDRENS
-    when 2
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    movie.charge(days_rented)
   end
 
   def frequent_renter_points
-    # add bonus for a two day new release rental
-    # movie.price_code == (Movie.NEW_RELEASE && days_rented > 1) ? 2 : 1
-    movie.price_code == (1 && days_rented > 1) ? 2 : 1
+    movie.frequent_renter_points(days_rented)
   end
 end
