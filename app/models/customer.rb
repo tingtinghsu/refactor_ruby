7cb4a093640e7e55d0e3b4ee14e8a8bc115acbaf
tingtinghsu <tingtinghsu@Hui-TingdeMacBook-Pro.local>
2023-05-03 00:06:58 +0800

    Remove old method Client#amount_for and template variable this_amount

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -54,8 +54,6 @@ class Customer
 
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = amount_for(element)
-
       # add frequent renter points
       frequent_renter_points += 1
 
@@ -66,16 +64,12 @@ class Customer
         frequent_renter_points += 1
       end
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+      total_amount += element.charge
     end
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
-  end
-
-  def amount_for(rental)
-    rental.charge
   end
 end
